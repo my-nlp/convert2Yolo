eb49415ad66bfafbcbec41dc792adfe0a2195d12
xzming <xzm@umshz.com>
2019-07-20 10:49:46 +0800

    add voc2yolo script

---
+++ convertVOC2Yolo.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python3 example.py --datasets VOC --img_path /Users/xzm/Downloads/AppleImages/Images/ --label /Users/xzm/Downloads/AppleImages/Labels/ --convert_output_path  /Users/xzm/Downloads/AppleImages/CVT_Yolo/ --img_type ".jpg" --manipast_path /Users/xzm/Downloads/AppleImages/ --cls_list_file /Users/xzm/Downloads/AppleImages/classes.txt
