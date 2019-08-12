b851f6954dff0519a1257c3b138e3519f673ac12
xzming <xzm@umshz.com>
2019-08-12 11:48:28 +0800

    update config

--- convertVOC2YoloS.sh
+++ convertVOC2YoloS.sh
@@ -1,4 +1,4 @@
 
 #! /bin/bash
 
-python3 example.py --datasets VOC --img_path ./voc_test_data/images/ --label ./voc_test_data/labels/ --convert_output_path  ./voc_test_data/yolo_data/ --img_type ".jpg" --manipast_path ./voc_test_data/yolo_data --cls_list_file ./voc_test_data/classes.txt
+python3 example.py --datasets VOC --img_path ./voc_test_data/images/ --label ./voc_test_data/labels/ --convert_output_path  ./voc_test_data/images/ --img_type ".jpg" --manipast_path ./voc_test_data/yolo_data --cls_list_file ./voc_test_data/classes.txt
